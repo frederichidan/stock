4fe42dd385ee9b07464ac8b2a5d672a140b4e900
MestourBS <42637780+MestourBS@users.noreply.github.com>
2019-03-22 09:17:08 +0100

    Add back to list button after deletion

--- application/views/inventory_control/success.php
+++ application/views/inventory_control/success.php
@@ -1,3 +1,4 @@
 <div class="container">
-    <?= $this->lang->line('delete_inventory_control_success'); ?>
+    <?= $this->lang->line('delete_inventory_control_success'); ?><br>
+    <a href="<?= base_url('item/inventory_controls/'.$item_id); ?>" class="btn"><?= lang('btn_back_to_list'); ?></a>
 </div>
